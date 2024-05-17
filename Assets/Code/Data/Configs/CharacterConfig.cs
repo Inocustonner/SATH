7da65987a35b93a6b697c14598112ce9e2cddac9
awdsasdaw <69734315+XXXawdsasdawXXX@users.noreply.github.com>
2024-05-17 22:23:14 +0300

    =)

--- Assets/Code/Data/Configs/CharacterConfig.cs
+++ Assets/Code/Data/Configs/CharacterConfig.cs
@@ -10,19 +10,19 @@ namespace Code.Data.Configs
     }
 
     [Serializable]
-    public class MovementData
+    public struct MovementData
     {
         [SerializeField, Range(0f, 20f)] [Tooltip("Maximum movement speed")]
-        public float MaxSpeed = 10f;
+        public float MaxSpeed;
 
         [SerializeField, Range(0f, 100f)] [Tooltip("How fast to reach max speed")]
-        public float MaxAcceleration = 52f;
+        public float MaxAcceleration;
 
         [SerializeField, Range(0f, 100f)] [Tooltip("How fast to stop after letting go")]
-        public float MaxDeceleration = 52f;
+        public float MaxDeceleration;
 
         [SerializeField, Range(0f, 100f)] [Tooltip("How fast to stop when changing direction")]
-        public float MaxTurnSpeed = 80f;
+        public float MaxTurnSpeed ;
 
         [SerializeField] [Tooltip("Friction to apply against movement on stick")]
         public float Friction;
