71771ae6b2bb50978c8723d5c3b1e5f08d810226
awdsasdaw <69734315+XXXawdsasdawXXX@users.noreply.github.com>
2024-05-29 15:38:57 +0300

    Shader

--- Assets/Code/Data/Enums/GamePartName.cs
+++ Assets/Code/Data/Enums/GamePartName.cs
@@ -7,7 +7,21 @@ namespace Code.Data.Enums
         None,
         Part_1__home,
         Part_1__school,
-        Part_1__factory
-        
+        Part_1__factory,
+        part_2__friendship,
+        part_2__love,
+        part_2__back_to_factory,
+        part_2__disown_friendship,
+        part_2__disown_love,
+        part_2__disown_choice,
+        part_2__bad_ending_friendship,
+        part_2__bad_ending_love,
+        part_2__bad_ending_disownage,
+        part_3__phantasmagoria,
+        part_3__endeavours,
+        part_3__endeavours_genesis,
+        part_3__endeavours_destruction,
+        part_3__ending_unlucky,
+        part_3__ending_success
     }
 }
