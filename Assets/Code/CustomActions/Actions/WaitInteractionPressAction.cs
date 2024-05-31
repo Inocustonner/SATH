9c9e10b37a5b037a007496958f3c955adffbdaaf
awdsasdaw <69734315+XXXawdsasdawXXX@users.noreply.github.com>
2024-05-31 11:31:46 +0300

    School window animation

---
+++ Assets/Code/CustomActions/Actions/WaitInteractionPressAction.cs
@@ -0,0 +1,44 @@
+﻿using Code.Infrastructure.DI;
+using Code.Infrastructure.GameLoop;
+using Code.Infrastructure.Services;
+using UnityEngine;
+
+namespace Code.CustomActions.Actions
+{
+    public class WaitInteractionPressAction: CustomAction, IGameInitListener
+    {
+        [Header("Components")]
+        [SerializeField] private CustomAction[] _waitingActions;
+        [SerializeField] private CustomAction[] _resultActions;
+
+        [Header("Services")]
+        private InputService _inputService;
+
+        [Header("Dimanyc value")] 
+        private bool _isPressInteraction;
+        
+        public void GameInit()
+        {
+            _inputService = Container.Instance.FindService<InputService>();
+        }
+
+        public override void StartAction()
+        {
+            _inputService.OnPressInteractionKey += OnPressInteractionKey;
+        }
+
+        public override void StopAction()
+        {
+            _inputService.OnPressInteractionKey -= OnPressInteractionKey;
+            InvokeEndEvent();
+            if (_isPressInteraction)
+            {
+                
+            }
+        }
+
+        private void OnPressInteractionKey()
+        {
+        }
+    }
+}
