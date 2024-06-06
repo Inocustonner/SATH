d28174327cac57ca54aee8a7afeaa1a60989b4c9
awdsasdaw <69734315+XXXawdsasdawXXX@users.noreply.github.com>
2024-06-06 12:32:14 +0300

    Bar clocks

---
+++ Assets/Code/GameParts/Components/FriendShip/BarClocks.cs
@@ -0,0 +1,33 @@
+﻿using Code.Data.Interfaces;
+using UnityEngine;
+
+namespace Code.GameParts.Components.FriendShip
+{
+    public class BarClocks: MonoBehaviour, IRestarable
+    {
+        [SerializeField] private ClocksArrow[] _clocksArrow;
+        [SerializeField] private Vector2Int _defaultTime;
+        
+        [ContextMenu("SetDefaultTime")]
+        public void Restart()
+        {
+            SetDefaultTime();
+        }
+
+        public void SetTime(int hour,int minutes)
+        {
+            for (int i = 0; i < _clocksArrow.Length; i++)
+            {
+                _clocksArrow[i].SetTime(hour, minutes);   
+            }
+        }
+        
+        private void SetDefaultTime()
+        {
+            for (int i = 0; i < _clocksArrow.Length; i++)
+            {
+                _clocksArrow[i].SetTime(_defaultTime.x, _defaultTime.y);
+            }
+        }
+    }
+}
--- Assets/Code/GameParts/Components/FriendShip/ClocksArrow.cs
+++ Assets/Code/GameParts/Components/FriendShip/ClocksArrow.cs
@@ -1,11 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Code.GameParts.Components.FriendShip
 {
-    public class ClocksArrow : MonoBehaviour
+    [Serializable]
+    public class ClocksArrow 
     {
         [SerializeField] private Transform _minArrow, _hourArrow;
+
+
+        private const float MIN_ANGLE = 6;
+        private const float HOUR_ANGLE = 30;
         
+        public void SetTime(int hour, int minutes)
+        {
+            var minuteAngle = minutes * MIN_ANGLE;
+            var hourAngle = (hour % 12) * HOUR_ANGLE + (minutes / 2f);
+            _minArrow.localRotation = Quaternion.Euler(0f, 0f, -minuteAngle);
+            _hourArrow.localRotation = Quaternion.Euler(0f, 0f, -hourAngle);
+        }
         
+   
     }
 }
