42c735e41af0ae3c433225cc42eff8ef0d8f25f8
awdsasdaw <69734315+XXXawdsasdawXXX@users.noreply.github.com>
2024-06-15 00:43:41 +0300

    Update CameraService.cs

--- Assets/Code/Infrastructure/Cameras/CameraService.cs
+++ Assets/Code/Infrastructure/Cameras/CameraService.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 
 namespace Code.Game.Components
 {
-    public class CameraService: IService
+    public class CameraService: IService, IGameInitListener
     {
         public Camera CurrentCamera { get; private set; }
 
         public bool IsMovement{ get; private set; }
+
+        public void GameInit()
+        {
+            CurrentCamera = Camera.main;
+        }
+
         public void SetCurrentCamera(Camera camera, bool isMovement)
         {
             CurrentCamera = camera;
